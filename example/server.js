f5da62326cae7ce82943a0feedf4a1073edc362e
tofferPika <i-toffer@wanadoo.fr>
2014-04-12 16:33:06 +0200

    Reflect api changes to server example and README.

--- example/server.js
+++ example/server.js
@@ -1,6 +1,3 @@
-var fs = require('fs')
-	,aircode = require('../lib/node-aircode');
-
 var host    = process.argv[2]
 	,project = process.argv[3];
 
@@ -9,51 +6,70 @@ if (!host || !project) {
 	process.exit();
 }
 
+var fs = require('fs')
+	,aircode = require('../lib/node-aircode');
+
 // Create a Documents folder to hold projects
 var docsPath = 'Documents';
 try {
 	fs.mkdirSync(docsPath);
 } catch(e) {}
 
-// Create AirCode proxy and sync it with the Documents folder
-var proxy = aircode(host);
-var documents = proxy.sync(docsPath);
+// Sync AirCode proxy the Documents folder
+var documents = aircode.sync(docsPath);
 
-// Output files changes and AirCode updates
+// Output files changes / AirCode updates
+// and listen for special eval.lua file which serve us for eval only
 documents
 .on('fileAdded',function(subject) {
 	console.log('file added:',subject.file);
+	if (subject.buffer == 'eval') {
+		// prevent default behavior (Codea would Create new tab called eval)
+		subject.preventDefault = true;
+		// manualy fire eval only (no save)
+		aircode.eval(subject.contents,function(err) {
+			console.log(err ? err : 'Eval success');
+		});
+	}
 })
-.on('bufferAdded',function(subject) {
-	console.log('↳ AirCode created',subject.project + ':' + subject.buffer);
+.on('bufferAdded',function(subject,err) {
+	console.log(err ? '! AirCode error' : '↳ AirCode created',subject.project + ':' + subject.buffer);
 })
 .on('fileChanged',function(subject) {
 	console.log('file changed:',subject.file);
+	if (subject.buffer == 'eval') {
+		// prevent default behavior (Codea would save the tab)
+		subject.preventDefault = true;
+		// manualy fire eval only (no save)
+		aircode.eval(subject.contents,function(err) {
+			console.log(err ? err : 'Eval success');
+		});
+	}
 })
-.on('bufferChanged',function(subject) {
-	console.log('↳ AirCode updated',subject.project + ':' + subject.buffer);
+.on('bufferChanged',function(subject,err) {
+	console.log(err ? '! AirCode error' : '↳ AirCode updated',subject.project + ':' + subject.buffer);
 })
 .on('fileRemoved',function(subject) {
 	console.log('file removed:',subject.file);
 })
-.on('bufferRemoved',function(subject) {
-	console.log('↳ AirCode removed',subject.project + ':' + subject.buffer);
+.on('bufferRemoved',function(subject,err) {
+	console.log(err ? '! AirCode error' : '↳ AirCode removed',subject.project + ':' + subject.buffer);
 });
 
 // Start server and open the project
 // (loop until AirCode is launched, assuming a valid host address).
 console.log('Waiting connection...');
-proxy.connect(function handleConnect(err,projectsList) {
+aircode.connect(host,function handleConnect(err,projectsList) {
 	if (err) {
 		setTimeout(function() {
-			proxy.connect(handleConnect);
+			aircode.connect(handleConnect);
 		},600)
 		return;
 	}
 
 	console.log('Opening project:',project);
 
-	proxy.openProject(project,function(err) {
+	aircode.openProject(project,function(err) {
 		if (err)
 			return console.log(err);
 		console.log('→ project\'s files up to date in Documents/' + project + ', you should start coding !');
